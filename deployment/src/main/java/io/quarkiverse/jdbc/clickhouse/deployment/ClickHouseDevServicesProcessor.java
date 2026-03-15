ebd08128498f5ad51b60aec81b22909cb5ca14b0
Alexey Sharandin <asharandin@itkey.com>
2026-03-15 21:32:21 +0300

    remove quarkus-junit4-mock

--- deployment/src/main/java/io/quarkiverse/jdbc/clickhouse/deployment/ClickHouseDevServicesProcessor.java
+++ deployment/src/main/java/io/quarkiverse/jdbc/clickhouse/deployment/ClickHouseDevServicesProcessor.java
@@ -1,9 +1,18 @@
 package io.quarkiverse.jdbc.clickhouse.deployment;
 
+import static io.quarkiverse.jdbc.clickhouse.deployment.JdbcClickhouseProcessor.DRIVER_NAME;
+import static io.quarkus.datasource.deployment.spi.DatabaseDefaultSetupConfig.DEFAULT_DATABASE_NAME;
+import static io.quarkus.datasource.deployment.spi.DatabaseDefaultSetupConfig.DEFAULT_DATABASE_PASSWORD;
+import static io.quarkus.datasource.deployment.spi.DatabaseDefaultSetupConfig.DEFAULT_DATABASE_USERNAME;
+
 import java.time.Duration;
 import java.util.List;
 import java.util.Optional;
 import java.util.OptionalInt;
+
+import org.jboss.logging.Logger;
+import org.testcontainers.containers.ClickHouseContainer;
+import org.testcontainers.utility.DockerImageName;
 
 import io.quarkus.datasource.common.runtime.DataSourceUtil;
 import io.quarkus.datasource.deployment.spi.DatasourceStartable;
@@ -17,14 +26,6 @@ import io.quarkus.devservices.common.ConfigureUtil;
 import io.quarkus.devservices.common.Labels;
 import io.quarkus.devservices.common.Volumes;
 import io.quarkus.runtime.LaunchMode;
-import org.jboss.logging.Logger;
-import org.testcontainers.containers.ClickHouseContainer;
-import org.testcontainers.utility.DockerImageName;
-
-import static io.quarkiverse.jdbc.clickhouse.deployment.JdbcClickhouseProcessor.DRIVER_NAME;
-import static io.quarkus.datasource.deployment.spi.DatabaseDefaultSetupConfig.DEFAULT_DATABASE_NAME;
-import static io.quarkus.datasource.deployment.spi.DatabaseDefaultSetupConfig.DEFAULT_DATABASE_PASSWORD;
-import static io.quarkus.datasource.deployment.spi.DatabaseDefaultSetupConfig.DEFAULT_DATABASE_USERNAME;
 
 public class ClickHouseDevServicesProcessor {
 
@@ -50,8 +51,8 @@ public class ClickHouseDevServicesProcessor {
 
             @Override
             public DatasourceStartable createDatasourceStartable(Optional<String> username, Optional<String> password,
-                                                                 String datasourceName, DevServicesDatasourceContainerConfig containerConfig,
-                                                                 LaunchMode launchMode, boolean useSharedNetwork, Optional<Duration> startupTimeout) {
+                    String datasourceName, DevServicesDatasourceContainerConfig containerConfig,
+                    LaunchMode launchMode, boolean useSharedNetwork, Optional<Duration> startupTimeout) {
                 QuarkusClickHouseSQLContainer container = new QuarkusClickHouseSQLContainer(containerConfig.getImageName(),
                         containerConfig.getFixedExposedPort(),
                         !devServicesSharedNetworkBuildItem.isEmpty());
@@ -60,7 +61,7 @@ public class ClickHouseDevServicesProcessor {
                 String effectiveUsername = containerConfig.getUsername().orElse(username.orElse(DEFAULT_DATABASE_USERNAME));
                 String effectivePassword = containerConfig.getPassword().orElse(password.orElse(DEFAULT_DATABASE_PASSWORD));
                 String effectiveDbName = containerConfig.getDbName()
-                        .orElse(DataSourceUtil.isDefault(datasourceName) ? DEFAULT_DATABASE_NAME:datasourceName);
+                        .orElse(DataSourceUtil.isDefault(datasourceName) ? DEFAULT_DATABASE_NAME : datasourceName);
 
                 container.withUsername(effectiveUsername)
                         .withPassword(effectivePassword)
@@ -79,7 +80,6 @@ public class ClickHouseDevServicesProcessor {
                 return container;
             }
 
-
             @Override
             public Optional<DevServicesDatasourceProvider.RunningDevServicesDatasource> findRunningComposeDatasource(
                     LaunchMode launchMode, boolean useSharedNetwork, DevServicesDatasourceContainerConfig containerConfig,
@@ -87,8 +87,7 @@ public class ClickHouseDevServicesProcessor {
                 // No compose support
                 return Optional.empty();
             }
-        }
-        );
+        });
     }
 
     private static class QuarkusClickHouseSQLContainer extends ClickHouseContainer implements DatasourceStartable {
@@ -98,7 +97,7 @@ public class ClickHouseDevServicesProcessor {
         private String hostName = null;
 
         public QuarkusClickHouseSQLContainer(Optional<String> imageName, OptionalInt fixedExposedPort,
-                                             boolean useSharedNetwork) {
+                boolean useSharedNetwork) {
             super(DockerImageName
                     .parse(imageName.orElseGet(() -> ConfigureUtil.getDefaultImageNameFor("clickhouse")))
                     .asCompatibleSubstituteFor(DockerImageName.parse(ClickHouseContainer.IMAGE)));
@@ -147,7 +146,6 @@ public class ClickHouseDevServicesProcessor {
         public String getReactiveUrl() {
             return getEffectiveJdbcUrl().replaceFirst("jdbc:", "vertx-reactive:");
         }
-
 
         @Override
         public void close() {
