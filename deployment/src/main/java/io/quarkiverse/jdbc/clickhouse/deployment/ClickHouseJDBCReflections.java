d6d7219fd554e95d76abc5e8c12a31e4b55fde30
Alexey Sharandin <asharandin@itkey.com>
2026-04-08 17:26:01 +0300

    add LZ4 reflections

--- deployment/src/main/java/io/quarkiverse/jdbc/clickhouse/deployment/ClickHouseJDBCReflections.java
+++ deployment/src/main/java/io/quarkiverse/jdbc/clickhouse/deployment/ClickHouseJDBCReflections.java
@@ -16,11 +16,33 @@ import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
 
 @SuppressWarnings("unused")
 public class ClickHouseJDBCReflections {
+    private static final String[] LZ4_DYNAMIC_CLASSES = {
+            "com.clickhouse.client.internal.net.jpountz.lz4.LZ4JNICompressor",
+            "com.clickhouse.client.internal.net.jpountz.lz4.LZ4HCJNICompressor",
+            "com.clickhouse.client.internal.net.jpountz.lz4.LZ4JNIFastDecompressor",
+            "com.clickhouse.client.internal.net.jpountz.lz4.LZ4JNISafeDecompressor",
+            "com.clickhouse.client.internal.net.jpountz.lz4.LZ4JavaSafeCompressor",
+            "com.clickhouse.client.internal.net.jpountz.lz4.LZ4HCJavaSafeCompressor",
+            "com.clickhouse.client.internal.net.jpountz.lz4.LZ4JavaSafeFastDecompressor",
+            "com.clickhouse.client.internal.net.jpountz.lz4.LZ4JavaSafeSafeDecompressor",
+            "com.clickhouse.client.internal.net.jpountz.lz4.LZ4JavaUnsafeCompressor",
+            "com.clickhouse.client.internal.net.jpountz.lz4.LZ4HCJavaUnsafeCompressor",
+            "com.clickhouse.client.internal.net.jpountz.lz4.LZ4JavaUnsafeFastDecompressor",
+            "com.clickhouse.client.internal.net.jpountz.lz4.LZ4JavaUnsafeSafeDecompressor"
+    };
+
     @BuildStep
     void build(BuildProducer<ReflectiveClassBuildItem> reflectiveClass) {
         //Not strictly necessary when using Agroal, as it also registers
         //any JDBC driver being configured explicitly through its configuration.
         //We register it for the sake of people not using Agroal.
         reflectiveClass.produce(ReflectiveClassBuildItem.builder(DATA_SOURCE_NAME, DRIVER_NAME).constructors().build());
+        // ClickHouse's shaded LZ4 factory constructs implementation class names dynamically and
+        // loads them via ClassLoader at runtime, so native-image cannot discover them statically.
+        reflectiveClass.produce(ReflectiveClassBuildItem.builder(LZ4_DYNAMIC_CLASSES)
+                .constructors()
+                .methods()
+                .fields()
+                .build());
     }
 }
